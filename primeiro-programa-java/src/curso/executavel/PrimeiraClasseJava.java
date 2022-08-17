42d2245f1b61ded4e3277d87f2578334fdefef2e
Gabriel Elias <gabriel.elias2824@gmail.com>
2022-08-16 23:52:24 -0300

    Criação do projeto

---
+++ primeiro-programa-java/src/curso/executavel/PrimeiraClasseJava.java
@@ -0,0 +1,8 @@
+package curso.executavel;
+
+public class PrimeiraClasseJava {
+	/*Meu Deus, meu senhor, me ajuda, por favor*/
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java");
+	}
+}
